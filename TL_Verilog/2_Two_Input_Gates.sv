6bcb6cfc877952eee92fa676fcf7076df2fde26b
Maryam <maryam.pourbahman99@gmail.com>
2026-07-11 16:51:38 +0200

    Define two-input logic gates in SystemVerilog

--- TL_Verilog/2_Two_Input_Gates.sv
+++ TL_Verilog/2_Two_Input_Gates.sv
@@ -1,2 +1,17 @@
 // && means AND, || means OR, ^ means XOR.
 // In SystemVerilog, inputs and outputs must be declared explicitly.
+
+
+module two_input_gates (
+   input  logic in1,
+   input  logic in2,
+   output logic and_out,
+   output logic or_out,
+   output logic xor_out
+);
+
+   assign and_out = in1 && in2;
+   assign or_out  = in1 || in2;
+   assign xor_out = in1 ^ in2;
+
+endmodule
